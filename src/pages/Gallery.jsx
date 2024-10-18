ce35c0c917925fc040d8ab166010e6a1e378a19b
Nikhil Adiga <nikhil@sclera.com>
2024-10-18 10:06:34 +0530

    Added gallery component and two new npm packages

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { RowsPhotoAlbum } from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import "react-photo-album/rows.css";
+import "yet-another-react-lightbox/styles.css";
+import { Box } from "@chakra-ui/react";
+import { slides } from "../constants";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <Box width="90%" margin="auto" marginTop="10">
+      <RowsPhotoAlbum
+        onClick={({ index: current }) => setIndex(current)}
+        targetRowHeight={150}
+        photos={slides}
+      />
+      <Lightbox
+        index={index}
+        slides={slides}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+      />
+    </Box>
+  );
 };
 
 export default Gallery;
